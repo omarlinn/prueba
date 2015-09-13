7f1f94a088555eff3d1d98171d9b82a4da15d31a
omarlinn <omarlinneftali@hotmail.com>
2015-09-13 08:29:58 -0400

    paella

---
+++ tareas/metodos.java
@@ -0,0 +1,13 @@
+
+public class metodos  {
+
+    
+    public  double numeroDobles () {
+       return 3633.7895486756876575575777475; 
+        
+}
+    public float numeroFlotante(){
+    return 980.436546F;
+    }
+    
+}
---
+++ tareas/tareauno.java
@@ -0,0 +1,29 @@
+
+
+public class tareauno{
+public static void main(String[] args) {
+ byte bite=126;
+short pequeño=4567;
+int numero=5645647;
+long grande=4256436663663L;
+double dobles=6.434353454363636;
+float flotante=5.45555F;
+String nombre="hola";
+boolean boleano=true;
+char caracter='t';
+System.out.println("este valor es de tipo Byte: "+bite);
+System.out.println("este valor es de tipo short: "+pequeño);
+System.out.println("este valor es de tipo int: "+numero);
+System.out.println("este valor es de tipo long: "+grande);
+System.out.println("este valor es de tipo float: "+flotante);
+System.out.println("este valor es de tipo double: "+dobles);
+System.out.println("este valor es de tipo String:"+nombre);
+System.out.println("este valor es de tipo boolean: "+boleano);
+System.out.println("este valor es de tipo char: "+caracter);
+metodos devolver= new metodos();
+System.out.println(devolver.numeroDobles());
+System.out.println(devolver.numeroFlotante());
+
+    }
+    
+}
